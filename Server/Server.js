fd06a7225d463779a8a1b360042146e593971b64
Maxwell Todd <mtmaxtodd@gmail.com>
2021-02-23 11:11:22 -0600

    Fixed the get request and the api request to properly show data on the server on the terminal.

--- Server/Server.js
+++ Server/Server.js
@@ -1,19 +1,16 @@
-const express = require('express');
 require('dotenv').config();
-
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('build'));
 
 const weatherRouter = require('./Weather.router');
 
-app.use('/api/weather', weatherRouter);
-
-app.use(express.static('build'));
-
-const PORT = process.env.PORT || 5000;
+app.use('/weather', weatherRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
--- Server/Weather.router.js
+++ Server/Weather.router.js
@@ -2,17 +2,15 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const API_KEY = process.env.API_KEY;
-const city_name = 'Minneapolis'
-const state_code = 'MN';
-const BASE_URL = `api.openweathermap.org/data/2.5/weather?q=${city_name},${state_code}&appid=${API_KEY}`;
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=minneapolis&units=imperial&appid=${API_KEY}`;
 
 router.get('/', (req, res) => {
     axios({
         method: 'GET',
         url: `${BASE_URL}`
     }).then((response) => {
-        console.log(response);
-        res.send(response.data.data);
+        console.log(response.data);
+        //res.send(response);
     }).catch((error) => {
         console.log('Error in GET', error);
         res.sendStatus(500);
